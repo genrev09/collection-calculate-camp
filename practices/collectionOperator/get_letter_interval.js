cffafe9ccb66506e86c0159cc1b2e8b3a372945e
Genrev Arambulo <genrev.arambulo@oocl.com>
2019-10-12 23:36:56 +0800

    Committed finished get letter interval 1 & 2

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -1,7 +1,16 @@
 'use strict';
 
 function get_letter_interval(number_a, number_b) {
-  //write code here
+  let minimum = Math.min(number_a,number_b);
+  const maximum = Math.max(number_a,number_b);
+  const length = (maximum - minimum) +1;
+
+  const range = Array.from(Array(length),(x,y) => minimum++);
+
+  if(number_a > number_b)
+    range.sort((a,b) => b-a);
+  
+  return range.map(number => String.fromCharCode(number+96));
 }
 
 module.exports = get_letter_interval;
--- practices/collectionOperator/get_letter_interval_2.js
+++ practices/collectionOperator/get_letter_interval_2.js
@@ -1,7 +1,29 @@
 'use strict';
 
 function get_letter_interval_2(number_a, number_b) {
-  //write code here
+  let minimum = Math.min(number_a,number_b);
+  const maximum = Math.max(number_a,number_b);
+  const length = (maximum - minimum) +1;
+
+  const range = Array.from(Array(length),(x,y) => minimum++);
+
+  if(number_a > number_b)
+    range.sort((a,b) => b-a);
+  
+  return range.map(number => { 
+    let quotient = Math.trunc(number / 27);
+    let remaining = number - (quotient * 26);
+    
+    if(remaining === 27){
+      quotient+=1;
+      remaining = 1;
+    }
+    
+    if (quotient === 0)
+      return String.fromCharCode(number+96);
+    else
+      return String.fromCharCode(quotient+96) + String.fromCharCode(remaining+96);
+  });
 }
 
 module.exports = get_letter_interval_2;
