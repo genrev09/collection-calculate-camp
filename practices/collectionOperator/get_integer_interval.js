0ad53970e44b1d2aba32fb5524f0c1b3b54879ca
Genrev Arambulo <genrev.arambulo@oocl.com>
2019-10-12 22:52:35 +0800

    Completed get integer interval 1 & 2

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -1,7 +1,16 @@
 'use strict';
 
 function get_integer_interval(number_a, number_b) {
-  //write code here
+  let minimum = Math.min(number_a,number_b);
+  const maximum = Math.max(number_a,number_b);
+  const length = (maximum - minimum) +1;
+
+  const range = Array.from(Array(length),(x,y) => minimum++);
+
+  if(number_a <= number_b)
+    return range;
+  else
+    return range.sort((a,b) => b-a);
 }
 
 module.exports = get_integer_interval;
--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,7 +1,17 @@
 'use strict';
 
 function get_integer_interval_2(number_a, number_b) {
-  //write code here
+  let minimum = Math.min(number_a,number_b);
+  const maximum = Math.max(number_a,number_b);
+  const length = (maximum - minimum) +1;
+
+  const range = Array.from(Array(length),(x,y) => minimum++)
+                     .filter(number => number%2 === 0);
+
+  if(number_a <= number_b)
+    return range;
+  else
+    return range.sort((a,b) => b-a);
 }
 
 module.exports = get_integer_interval_2;
