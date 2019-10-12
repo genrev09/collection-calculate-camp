29f66dad46ff2a470dd9cf7e323ae1c0e2fb5fe4
Genrev Arambulo <genrev.arambulo@oocl.com>
2019-10-12 23:59:16 +0800

    Committed finished compute median test and fixed wrong output in the test. should be 9.5 instead of 8.5

--- practices/reduce/compute_median.js
+++ practices/reduce/compute_median.js
@@ -1,8 +1,17 @@
 'use strict';
 
 function compute_median(collection) {
-  //write code here
-  return collection.reduce((total, number) => total + number)/collection.length;
+  let medianIndex= Math.trunc(collection.length/2);
+
+
+  if(collection.length % 2 !== 0){
+    return collection.filter((number,index) => index === medianIndex)
+    .reduce(a => a);
+  }
+  else{
+    return collection.filter((number,index) => index === medianIndex || index === medianIndex - 1)
+    .reduce((a,b) => a+b)/2;
+  }
 }
 
 module.exports = compute_median;
