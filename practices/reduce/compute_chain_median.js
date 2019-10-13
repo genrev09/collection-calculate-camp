a16f0c60f6b4d748e3c2ea228d8c05644c50b2f3
Genrev Arambulo <genrev.arambulo@oocl.com>
2019-10-13 11:27:02 +0800

    Completed chain median test

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -1,7 +1,18 @@
 'use strict';
 
 function compute_chain_median(collection) {
-  //write code here
+  let number = collection.split('->');
+  let medianIndex= Math.trunc(number.length/2);
+  number = number.map(number => parseInt(number,10)).sort((a,b) => a-b);
+  
+  if(number.length % 2 !== 0){
+    return number.filter((value,index) => index === medianIndex)
+    .reduce(a => a);
+  }
+  else{
+    return number.filter((value,index) => index === medianIndex || index === medianIndex - 1)
+    .reduce((a,b) => a+b)/2;
+  }
 }
 
 module.exports = compute_chain_median;
